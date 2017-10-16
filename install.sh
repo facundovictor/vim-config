c1dc7fc966125dc15226e1552e6e60025356f525
Facundo Victor <facundovt@gmail.com>
2017-10-16 01:46:29 -0300

    Fix dependencies

--- install.sh
+++ install.sh
@@ -27,15 +27,15 @@ function ensure_dependencies () {
 	mkdir -p "${INSTALL_DIR}plugged/"
 	find $INSTALL_DIR -type d -exec chmod 777 \{} \;
 
-	dependencies=("
-		automake
-		gcc
-		gcc-c++
-		kernel-devel
-		cmake
-		python-devel
-		python3-devel
-	")
+	dependencies=(
+		"automake"
+		"gcc"
+		"gcc-c++"
+		"kernel-devel"
+		"cmake"
+		"python-devel"
+		"python3-devel"
+	)
 
 	dnf install "${dependencies[@]}"
 }
